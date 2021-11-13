95021bfed3652862bed1d8da549bdcbe480d5704
Anksharskarp <aerialconnection98@gmail.com>
2021-11-13 16:23:46 -0500

    Initial commit

---
+++ Raindrops/desktop/src/dev/anksharskarp/raindrops/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dev.anksharskarp.raindrops.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dev.anksharskarp.raindrops.Raindrops;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Raindrops(), config);
+	}
+}
