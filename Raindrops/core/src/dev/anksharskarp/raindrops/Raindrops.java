e31345a7c80b1ac8f2641d292bfb854d42257815
Anksharskarp <aerialconnection98@gmail.com>
2021-12-18 17:49:13 -0500

    Finished the basic Raindrops game. Be careful about instantiating objects; a lot of time was spent on debugging context errors and access errors because two LwjglApplication objects were created in the DesktopLauncher.java class by mistake!

--- Raindrops/core/src/dev/anksharskarp/raindrops/Raindrops.java
+++ Raindrops/core/src/dev/anksharskarp/raindrops/Raindrops.java
@@ -1,31 +1,137 @@
 package dev.anksharskarp.raindrops;
 
+import java.util.Iterator;
+
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class Raindrops extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private Texture dropImage;
+	private Texture bucketImage;
+	private Sound dropSound;
+	private Music rainMusic;
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Rectangle bucket;
+	private Array<Rectangle> raindrops;
+	private long lastDropTime;
 	
 	@Override
 	public void create () {
+		//Load the images for the droplet and the bucket, 64x64 pixels each
+		dropImage = new Texture(Gdx.files.internal("droplet.png"));
+		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+		//Load the drop sound effect and the rain background "music"
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+		//Start the playback of the background music immediately
+		rainMusic.setLooping(true);
+		rainMusic.play();
+
+		//Create the camera and the SpriteBatch
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		//Create a Rectangle to logically represent the bucket
+		bucket = new Rectangle();
+		bucket.x = 800 / 2 - 64 / 2; //Center the bucket horizontally
+		bucket.y = 20; //Bottom left corner of the bucket is 20 pixels above the bottom screen edge
+		bucket.width = 64;
+		bucket.height = 64;
+
+		//Create the raindrops array and spawn the first raindrop
+		raindrops = new Array<Rectangle>();
+		spawnRaindrop();
+	}
+
+	private void spawnRaindrop() {
+		Rectangle raindrop = new Rectangle();
+		raindrop.x = MathUtils.random(0, 800 - 64);
+		raindrop.y = 480;
+		raindrop.width = 64;
+		raindrop.height = 64;
+		raindrops.add(raindrop);
+		lastDropTime = TimeUtils.nanoTime();
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		//Clear the screen with a dark blue color. The
+		//arguments to clear are the red, green
+		//blue and alpha component in the range [0,1]
+		//of the color to be used to clear the screen.
+		ScreenUtils.clear(0, 0, 0.2f, 1);
+
+		//Tell the camera to update its matrices.
+		camera.update();
+
+		//Tell the SpriteBatch to render in the
+		//coordinate system specified by the camera.
+		batch.setProjectionMatrix(camera.combined);
+
+		//Begin a new batch and draw the bucket and
+		//all drops
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(bucketImage, bucket.x, bucket.y);
+		for(Rectangle raindrop : raindrops) {
+			batch.draw(dropImage, raindrop.x, raindrop.y);
+		}
 		batch.end();
+
+		//Process user input
+		if(Gdx.input.isTouched()) {
+			Vector3 touchPos = new Vector3();
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			bucket.x = touchPos.x - 64 / 2;
+		}
+		if(Gdx.input.isKeyPressed(Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
+
+		//Make sure the bucket stays within the screen bounds
+		if(bucket.x < 0) bucket.x = 0;
+		if(bucket.x > 800 - 64) bucket.x = 800 - 64;
+
+		//Check if we need to create a new raindrop
+		if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
+
+		//Move the raindrops, remove any that are beneath the bottom edge of
+		//the screen or that hit the bucket. In the latter case we play back
+		//a sound effect as well.
+		for(Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext(); ) {
+			Rectangle raindrop = iter.next();
+			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+			if(raindrop.y + 64 < 0) iter.remove();
+			if(raindrop.overlaps(bucket)) {
+				dropSound.play();
+				iter.remove();
+			}
+		}
 	}
+
+
 	
 	@Override
 	public void dispose () {
+		dropImage.dispose();
+		bucketImage.dispose();
+		dropSound.dispose();
+		rainMusic.dispose();
 		batch.dispose();
-		img.dispose();
 	}
 }
--- Raindrops/desktop/src/dev/anksharskarp/raindrops/desktop/DesktopLauncher.java
+++ Raindrops/desktop/src/dev/anksharskarp/raindrops/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import dev.anksharskarp.raindrops.Raindrops;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Raindrops";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new Raindrops(), config);
 	}
 }
